126094db7efe72d471ec078918f0057c0d92415a
slogbeast <junk@thoughtphul.com>
2013-07-05 18:45:17 -0400

    base class for deaths

--- MyApplication/src/main/java/net/slogland/horribledeath/DeathGenerator.java
+++ MyApplication/src/main/java/net/slogland/horribledeath/DeathGenerator.java
@@ -7,7 +7,7 @@ import android.R;
  * Created by root on 7/5/13.
  * tproy
  *
- * inteded for use with a toy app for android
+ * intended for use with a toy app for android
  * gives a random death that may kill you today
  * based on CDC gross death rate tables
  */
@@ -23,8 +23,7 @@ public class DeathGenerator {
 
     public int AddDeath(R.string name, R.string desc)
     {
-        int index = UpdateDeathList(name, desc);
-        return index;
+        return UpdateDeathList(name, desc);
     }
 
     private int UpdateDeathList(R.string n, R.string desc)
